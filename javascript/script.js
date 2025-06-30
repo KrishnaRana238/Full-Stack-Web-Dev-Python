28b0e8c8177e18ddd47d49a4d99373391d702fc9
Krishna Rana <rana345212@gmail.com>
2025-07-01 01:38:27 +0530

    change6

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('mybutton');
+const output = document.getElementById('output');
+
+
+button.addEventListener('click', () => {
+    output.textContent = 'Button clicked!';
+});
